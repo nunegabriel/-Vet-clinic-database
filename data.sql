9f37a01f4e24760b4a15f36efceba4e9732f5e7b
nunegabriel <nune.gabriel.gn@gmail.com>
2023-01-06 15:46:28 +0000

    Add Solution

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 /* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+ INSERT INTO animals (id,name,date_of_birth,weight_kg,neutered,escape_attempts) VALUES (1,'Agumon', '02-03-2020','10.23','true',0);
+  INSERT INTO animals (id,name,date_of_birth,weight_kg,neutered,escape_attempts) VALUES (2,'Gabumon', '11-15-2018','8','true',2);
+  INSERT INTO animals (id,name,date_of_birth,weight_kg,neutered,escape_attempts) VALUES (3,'Pikachu', '1-7-2021','15.04','false',1);
+  INSERT INTO animals (id,name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (4,'Devimon', '05-12-2017', '11', 'true', '5');

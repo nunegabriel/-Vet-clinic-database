c6b4c0a13f76a1138ab0a110a1d43f0c64cbac18
nunegabriel <nune.gabriel.gn@gmail.com>
2023-01-12 14:51:32 +0000

    ADD MEDICAL_HISTORIES TABLE

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,3 +6,12 @@ name VARCHAR(100),
 date_of_birth DATE NOT NULL,
 PRIMARY KEY(id),
 );
+
+CREATE TABLE medical_histories (
+  id SERIAL NOT NULL,
+  admitted_at TIMESTAMP NOT NULL,
+  patient_id INT NOT NULL,
+  status VARCHAR(100),
+  PRIMARY KEY (id)
+  FOREIGN KEY (patient_id) REFERENCES patients(id)
+);

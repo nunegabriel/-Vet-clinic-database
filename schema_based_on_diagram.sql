9c856512e2019bdff46f42a8c75247dbeddefda2
nunegabriel <nune.gabriel.gn@gmail.com>
2023-01-12 14:52:24 +0000

    ADD INVOICES TABLE

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -15,3 +15,11 @@ CREATE TABLE medical_histories (
   PRIMARY KEY (id)
   FOREIGN KEY (patient_id) REFERENCES patients(id)
 );
+
+CREATE TABLE invoices (
+    id SERIAL NOT NULL,
+    total_amount decimal,
+    generated_at TIMESTAMP,
+    paid_at TIMESTAMP,
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
+);
